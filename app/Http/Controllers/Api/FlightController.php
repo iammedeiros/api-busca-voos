94cc3ebd3ce12c036adbbf44b8a7e01340227092
Iam Medeiros <iam_medeiros@yahoo.com.br>
2020-05-08 19:22:02 -0300

    Implementa leitura dos dados no FlightController

--- app/Http/Controllers/Api/FlightController.php
+++ app/Http/Controllers/Api/FlightController.php
@@ -7,5 +7,41 @@ use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
-    //
+    private $flights;
+
+    public function __construct() {
+        $this->flights = self::read_data();
+    }
+
+    //função para ler os dados dos arquivos csv e json e armazena-los em um array json
+    private function read_data() {
+        $data = [];
+        $handle = fopen("data/uberair.csv", "r");
+        $row = 0;
+        
+        //lendo os dados do csv
+        while ($line = fgetcsv($handle, 1000, ",")) {
+            if ($row++ == 0) {
+                continue;
+            }
+            
+            $data[] = [
+                "voo" => $line[0],
+                "origem" => $line[1],
+                "destino" => $line[2],
+                "data_saida" => $line[3],
+                "saida" => $line[4],
+                "chegada" => $line[5],
+                "valor" => (float) $line[6]
+            ];
+        }
+        fclose($handle);
+        
+        //junta os dados do csv e do json e devolve como retorno da função
+        return array_merge($data, json_decode(file_get_contents('data/99planes.json')));
+    }
+
+    public function index(Request $request) {
+        return response()->json(['data' => $this->flights, 'lines' => count($this->flights)]);
+    }
 }
